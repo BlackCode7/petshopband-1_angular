6bf184e3aafc215b8d6deaecf1d79cc0f5dfe429
Anderson <BlackCode7@outlook.com>
2022-03-11 17:26:20 -0300

    manipulando objetos em listas com angular usando tabelas

--- frontend-angular/src/app/components/clientes/listar-clientes/listar-clientes.component.ts
+++ frontend-angular/src/app/components/clientes/listar-clientes/listar-clientes.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarClientesComponent implements OnInit {
 
+  // Criando uma lista de objetos e iterando sobre ela
+  listaClientes: any[] = [
+    {nome:'Anderson', endereco: 'endereco ficticio', email: 'email@email.com', telefone:"99065488", id:1},
+    {nome:'Simone', endereco: 'endereco Ficticio 2', email: 'Simone@Simone.com', telefone:"99065422", id:2},
+    {nome:'Joao', endereco: 'endereco Ficticio 3', email: 'Joao@Joao.com', telefone:"99165423", id:3}    
+    
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
