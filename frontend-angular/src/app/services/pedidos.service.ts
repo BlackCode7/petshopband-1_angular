94302d8a50c64a1cd92d874934f42d3074314f33
Anderson <BlackCode7@outlook.com>
2022-03-19 18:10:32 -0300

    criando funções de conexão com API get-post-put-delete

--- frontend-angular/src/app/services/pedidos.service.ts
+++ frontend-angular/src/app/services/pedidos.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { IPedido } from '../model/IPedido.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PedidosService {
 
-  constructor() { }
+
+export class PedidosService {
+  
+  private URL: string = 'http://localhost:3000/pedidos'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  buscarPedidos(): Observable<IPedido[]>{
+    return this.http.get<IPedido[]>(this.URL);
+  }
+
+  buscarPedidoPorID(id: number): Observable<IPedido>{
+    return this.http.get<IPedido>(`${this.URL}/${id}`);
+  }
+
+  salvarPedidos(pedido: IPedido): Observable<IPedido[]>{
+    return this.http.post<IPedido[]>(this.URL, pedido);
+  }
+
+  atualizarPedido(pedido: IPedido): Observable<IPedido[]>{
+    return this.http.put<IPedido[]>(`${this.URL}/${pedido.idPedido}`, pedido);
+  }
+
+  deletandoPedidos(id: number): Observable<IPedido[]>{
+    return this.http.delete<IPedido[]>(`${this.URL}/${id}`);
+  }
+
 }
